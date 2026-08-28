a379f53f82fbe08e06408a3e224e09a1ae7aeebf
Eishal <eishal.cecos@gmail.com>
2026-08-28 03:23:24 -0700

    Centralize current Meta Graph API version

---
+++ lib/meta.ts
@@ -0,0 +1,4 @@
+// Keep Meta Graph API versioning in one place. Update this when Meta ships a
+// new stable version and after verifying Messenger/Instagram/WhatsApp behavior.
+export const META_GRAPH_API_VERSION = "v26.0";
+export const META_GRAPH_BASE_URL = `https://graph.facebook.com/${META_GRAPH_API_VERSION}`;
